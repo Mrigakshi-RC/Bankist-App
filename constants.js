9418a7ad90d339a236bc7318d2cecd63fada5e2a
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-08-25 09:54:18 +0530

    refactor: move constants to another file

---
+++ constants.js
@@ -0,0 +1,25 @@
+export const USERS = [
+  {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    pin: 1111,
+  },
+
+  {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    pin: 2222,
+  },
+
+  {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    pin: 3333,
+  },
+
+  {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    pin: 4444,
+  },
+];
--- script.js
+++ script.js
@@ -1,37 +1,12 @@
 'use strict';
+import { USERS } from "./constants.js";
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300], 
-  interestRate: 1.2, // %
-  pin: 1111,
-};
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -90,7 +65,8 @@ const displayMovements=(movements)=>{
     containerMovements.insertAdjacentHTML("afterbegin", html);
   })
 }
-displayMovements(account1.movements)
+console.log(USERS[0].movements)
+displayMovements(USERS[0].movements)
 
 
 /////////////////////////////////////////////////
